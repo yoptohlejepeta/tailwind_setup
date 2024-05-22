828322da183dbfdafdf0d170e402ff44a850ee36
yoptohlejepeta <peta.kotlan@seznam.cz>
2024-05-22 16:52:44 +0200

    fastapi index

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI(
-    docs_url="/",
+    docs_url="/docs",
     redoc_url=None,
     title="TAILWIND Setup",
 )
 
 app.mount("/static", StaticFiles(directory="./static"), name="static")
+app.mount("/css", StaticFiles(directory="./css"), name="css")
 
 templates = Jinja2Templates(directory="./templates")
 
@@ -18,6 +19,6 @@ templates = Jinja2Templates(directory="./templates")
 async def read_root():
     return {"message": "Hello World"}
 
-# @app.get("/index")
-# async def read_root(request: Request):
-#     return templates.TemplateResponse("index.html", {"request": request})
+@app.get("/")
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
